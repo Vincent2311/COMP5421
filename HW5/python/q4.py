bd195103fc4da0d5d1bc5389ef789b714e63e6f5
Vincent2311 <zwanggk@connect.ust.hk>
2023-04-23 19:11:31 +0800

    testing Q4.4

--- HW5/python/q4.py
+++ HW5/python/q4.py
@@ -16,5 +16,19 @@ def findLetters(image):
     # insert processing in here
     # one idea estimate noise -> denoise -> greyscale -> threshold -> morphology -> label -> skip small boxes 
     # this can be 10 to 15 lines of code using skimage functions
+    image = skimage.restoration.denoise_bilateral(image,channel_axis=-1)
+    gray = skimage.color.rgb2gray(image)
     
+    # threshold & morphology
+    thresh = skimage.filters.threshold_otsu(gray)
+    bw = gray < thresh
+    bw = skimage.morphology.closing(bw)
+
+    # label, 0-valued pixels are considered as background pixels
+    label_image = skimage.morphology.label(bw, connectivity=2)
+    props = skimage.measure.regionprops(label_image)
+    mean_area = sum([x.area for x in props])/len(props)
+    bboxes = [x.bbox for x in props if x.area > mean_area/2]
+
+    bw = (~bw).astype(np.float)
     return bboxes, bw
--- HW5/python/run_q4.py
+++ HW5/python/run_q4.py
@@ -24,18 +24,56 @@ for img in os.listdir('../images'):
     im1 = skimage.img_as_float(skimage.io.imread(os.path.join('../images',img)))
     bboxes, bw = findLetters(im1)
 
-    plt.imshow(bw)
+    plt.imshow(bw, cmap='gray')
     for bbox in bboxes:
         minr, minc, maxr, maxc = bbox
         rect = matplotlib.patches.Rectangle((minc, minr), maxc - minc, maxr - minr,
                                 fill=False, edgecolor='red', linewidth=2)
         plt.gca().add_patch(rect)
     plt.show()
+
     # find the rows using..RANSAC, counting, clustering, etc.
+    mean_height = sum([b[2]-b[0] for b in bboxes])/len(bboxes)
+    #get the center_x, center_y, width, height 
+    centers = [((b[3]+b[1])//2,(b[2]+b[0])//2,b[3]-b[1],b[2]-b[0]) for b in bboxes]
+    centers = sorted(centers,key = lambda center: center[1])
+
+    rows = []
+    row = []
+    current_y = centers[0][1]
+    for center in centers:
+        if center[1] - current_y > mean_height:
+            row = sorted(row,key = lambda center: center[0])
+            rows.append(row)
+            row = [center]
+            current_y = center[1]
+        else:
+            row.append(center)
+    row = sorted(row,key = lambda center: center[0])
+    rows.append(row)
+    print("row size",len(rows[0]))
+
+    ##correct above
     
     # crop the bounding boxes
     # note.. before you flatten, transpose the image (that's how the dataset is!)
     # consider doing a square crop, and even using np.pad() to get your images looking more like the dataset
+    data = []
+    for row in rows:
+        line = []
+        for x,y,width,height in row:
+            cropped = bw[y-height//2:y+height//2, x-width//2:x+width//2]
+            if height > width:
+                padding=((0,0),(0,height-width))
+            else:
+                padding=((0,width-height),(0,0))
+            cropped = np.pad(cropped,padding,mode='constant',constant_values=(1, 1))
+            cropped = skimage.transform.resize(cropped, (32, 32))
+            line.append(cropped.T.flatten())
+        data.append(line)
+    print("data size",len(data))
+
+    ## correct below
     
     # load the weights
     # run the crops through your neural network and print them out
@@ -44,3 +82,11 @@ for img in os.listdir('../images'):
     letters = np.array([_ for _ in string.ascii_uppercase[:26]] + [str(_) for _ in range(10)])
     params = pickle.load(open('q3_weights.pickle','rb'))
     
+    for line in data:
+        h1 = forward(line,params,'layer1')
+        probs = forward(h1,params,'output',softmax)
+        pred_idx = np.argmax(probs, axis = -1)
+        strings = ''
+        for idx in pred_idx:
+            strings += letters[idx]
+        print(strings)
