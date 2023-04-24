3357378ee6d4cfac8313a87f570f0217e548901b
Vincent2311 <zwanggk@connect.ust.hk>
2023-04-24 17:03:04 +0800

    finished Q6

--- HW5/python/run_q6.py
+++ HW5/python/run_q6.py
@@ -2,7 +2,7 @@ import numpy as np
 import scipy.io
 from nn import *
 import matplotlib.pyplot as plt
-from skimage.measure import compare_psnr as psnr
+import skimage
 
 train_data = scipy.io.loadmat('../data/nist36_train.mat')
 valid_data = scipy.io.loadmat('../data/nist36_valid.mat')
@@ -12,25 +12,43 @@ train_x = train_data['train_data']
 valid_x = valid_data['valid_data']
 
 dim = 32
-# do PCA
+# do PCA on training set
+train_mean = np.sum(train_x,axis=0) / train_x.shape[0]
+train_x -= train_mean
+u, s, vh = np.linalg.svd(train_x)
+M = vh[:dim,:]
 
 # rebuild a low-rank version
-lrank = None
+train_lrank = train_x @ M.T
 
 # rebuild it
-recon = None
+train_recon = train_lrank @ M
+train_recon += train_mean
+train_x += train_mean
 
-for i in range(5):
+
+# do PCA on validation set
+valid_mean = np.sum(valid_x,axis=0) / valid_x.shape[0]
+valid_x -= valid_mean
+
+# rebuild a low-rank version
+valid_lrank = valid_x @ M.T
+
+# rebuild it
+valid_recon = valid_lrank @ M
+valid_recon += valid_mean
+valid_x += valid_mean
+
+
+selected_idx = [7, 36, 166, 188, 265, 276, 355, 384, 432, 496]
+for i in range(10):
     plt.subplot(2,1,1)
-    plt.imshow(train_x[i].reshape(32,32).T)
+    plt.imshow(valid_x[selected_idx[i]].reshape(32,32).T)
     plt.subplot(2,1,2)
-    plt.imshow(recon[i].reshape(32,32).T)
+    plt.imshow(valid_recon[selected_idx[i]].reshape(32,32).T)
     plt.show()
 
-# build valid dataset
-recon_valid = None
-
 total = []
-for pred,gt in zip(recon_valid,valid_x):
-    total.append(psnr(gt,pred))
+for pred,gt in zip(valid_recon,valid_x):
+    total.append(skimage.metrics.peak_signal_noise_ratio(gt,pred))
 print(np.array(total).mean())
